044d76ac06d1aae3fd00cb8e83e2bc9db33cbb54
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-06 04:04:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function findCodePositon(str) {
+    return str.indexOf("abc");
+}
+;
+console.log(findCodePositon("Learn to code with Typescript"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+function findCodePositon(str:string): number{
+    return str.indexOf("code")
+};
+
+console.log(findCodePositon("Learn to code with Typescript"));
